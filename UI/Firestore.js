612dc0b786a23f7412882e8e8dfc0e2dd037057f
Junior-Adadie <junioradadie@gmail.com>
2020-08-08 02:20:58 +0300

    Add retrieve codes and  references to firestore
    If applied, commit will add codes to retrive and send data to firestore
    from the blog post form and contact form

--- UI/Firestore.js
+++ UI/Firestore.js
@@ -1,50 +1,51 @@
 const regform = document.querySelector('.form');
-const contactform = document.querySelector("#contactform")
-const postform = document.querySelector('.post')
+const contactform = document.querySelector("#contactform");
+const postform = document.querySelector('#postform');
+const messagelist = document.querySelector(".messagelist");
 
-// saving SignUp data
-regform.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('Users').add({
-        fname: regform.fname.value,
-        lname: regform.lname.value,
-        email: regform.email.value,
-        paswword: regform.password.value,
-    });
-    regform.fname.value = '';
-    regform.lname.value = '';
-    regform.email.value = '';
-    regform.password.value = '';
-});
 
-//Saving questions
-contactform.addEventListener('submit', (e) => {
-    e.preventDefault();
+function send(e) {
+    e.preventDefault;
     db.collection('Messages').add({
         Names: contactform.names.value,
         email: contactform.email.value,
         Message: contactform.message.value,
         Comments: contactform.comments.value,
     });
-    form.names.value = '';
-    form.email.value = '';
-    form.message.value = '';
-    form.comments.value = '';
-});
+    contactform.reset();
+}
+document.getElementById('messagebtn').addEventListener('click', send);
 
 //Saving new blogs to firestore
-postform.addEventListener('submit', (e) => {
+function post(e) {
     e.preventDefault();
     db.collection('Posts').add({
         Author_Names: postform.authornames.value,
-        Title: postform.title.value,
-        Content: postform.content.value,
+        Description: postform.description.value,
+        Info: postform.info.value,
         Citation: postform.citation.value,
-        Cover_Image: postform.cover.value,
+       // Cover_Image: postform.cover.value,
+    })
+    postform.reset();
+}
+document.getElementById('postblog').addEventListener('click', post);
+
+//Retrieving Posts
+const posted = document.querySelector('.posted')
+db.collection("Posts").onSnapshot(function(querySnapshot) {
+    querySnapshot.docChanges().forEach(function(change) {
+        if (change.type === "added") {
+            posted.innerHTML += "<article class='article'><h2>" + change.doc.data().Author_Names + "</h2>  <h5>" + change.doc.data().Description + "</h5><p>" 
+        + change.doc.data().Info + "</p><p>" + change.doc.data().Citation +  "</p></section>"
+        }
     });
-    postform.authorname.value = '';
-    postform.title.value = '';
-    postform.content.value = '';
-    postform.citation.value = '';
-    postform.cover.value='';
+});
+       
+
+db.collection("Messages").onSnapshot(function(querySnapshot) {
+    querySnapshot.docChanges().forEach(function(change) {
+        if (change.type === "added") { messagelist.innerHTML += "<div class='list'><h3>" + change.doc.data().Names + "</h3> <p> Email:" + change.doc.data().email + "</p><p>" 
+        + change.doc.data().Message + "</p><p>" + change.doc.data().Comments +  "</p></div>"
+        }
+    });
 });
