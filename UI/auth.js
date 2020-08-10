10850f1dbc74f3ca0ab120e90b89cade2f3bb39b
Junior-Adadie <junioradadie@gmail.com>
2020-08-10 07:50:40 +0300

    Add authentication scripts to registration form

--- UI/auth.js
+++ UI/auth.js
@@ -32,3 +32,30 @@ auth.signInUserWithEmailAndPassword(email, password).then(cred => {
       loginForm.reset();
     });
 });
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  })
+
+  // login
+   // get user info
+   
+/*const email = loginForm['login-email'].value;
+const password = loginForm['login-password'].value;
+
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    
+//reset form
+    loginForm.reset();
+  })
+});*/
